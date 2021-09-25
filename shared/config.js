6498aa18727eff0f571287f9c4163d8fd6df4824
Jon Loureiro <me@jonloureiro.dev>
2021-09-25 14:19:22 -0400

    Development (#5)
    * add cors in env
    
    * rename folder
    
    * delete app2
    
    * rename app

--- shared/config.js
+++ shared/config.js
@@ -4,7 +4,8 @@ const config = {
   MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017',
   SECRET: process.env.SECRET || 's3cr3t',
   CLIENT: process.env.CLIENT || 'app1-tcc-jonloureiro.netlify.app',
-  DOMAIN_SSO: process.env.DOMAIN_SSO || 'accounts-tcc-jonloureiro-888075.netlify.live'
+  DOMAIN_SSO: process.env.DOMAIN_SSO || 'accounts-tcc-jonloureiro-db02e5.netlify.live',
+  CORS: process.env.CORS || 'http://localhost:8888,http://localhost:40000'
 }
 
 Object.freeze(config)
