53b2c796a80b3c5258aeddd414eedf7b55e0c298
Jon Loureiro <me@jonloureiro.dev>
2021-09-25 14:27:52 -0400

    fix domain sso env (#7)

--- shared/config.js
+++ shared/config.js
@@ -3,8 +3,7 @@
 const config = {
   MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017',
   SECRET: process.env.SECRET || 's3cr3t',
-  CLIENT: process.env.CLIENT || 'app1-tcc-jonloureiro.netlify.app',
-  DOMAIN_SSO: process.env.DOMAIN_SSO || 'accounts-tcc-jonloureiro-db02e5.netlify.live',
+  DOMAIN_SSO: process.env.DOMAIN_SSO || 'accounts.jonloureiro.dev',
   CORS: process.env.CORS || 'http://localhost:8888,http://localhost:40000'
 }
 
