c3d505559918bedeb98a3689cf779c01eaacb3d0
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2021-09-26 20:42:06 -0400

    add endpoint "cors"

---
+++ accounts/api/cors.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const httpResponse = require('../../shared/httpResponse')
+const config = require('../../shared/config')
+
+exports.handler = async function (event, context) {
+  if (context) context.callbackWaitsForEmptyEventLoop = false
+
+  if (event.httpMethod !== 'GET') return httpResponse.METHOD_NOT_ALLOWED
+
+  const cors = config.CORS.split(',')
+
+  try {
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ cors })
+    }
+  } catch (error) {
+    return httpResponse.UNAUTHORIZED
+  }
+}
