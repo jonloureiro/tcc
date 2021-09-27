d0c150c970f48f5eebc7cf7128b8caea8dac63a6
Jonathan Loureiro <jonathan.loureiro.m@gmail.com>
2021-09-27 00:57:52 -0400

    add redirect in accounts page

--- accounts/public/main.js
+++ accounts/public/main.js
@@ -1,6 +1,6 @@
 import { h, text, app } from 'https://unpkg.com/hyperapp@2.0.19/index.js'
 
-/* global fetch, Headers */
+/* global fetch, Headers, alert, atob */
 
 const OnSubmit = (state, event) => {
   event.preventDefault()
@@ -9,7 +9,7 @@ const OnSubmit = (state, event) => {
     ...state,
     username,
     password,
-    loading: true
+    message: 'Fetching...'
   }, [async dispatch => {
     const headers = new Headers()
     headers.append('Content-Type', 'application/json')
@@ -22,15 +22,22 @@ const OnSubmit = (state, event) => {
         password
       })
     }
-    const response = await fetch('/.netlify/functions/login', requestInit)
-    const { access_token: accessToken } = await response.json()
-    dispatch(StoppedLoading, { accessToken, username, password: '' })
+    try {
+      const response = await fetch('/.netlify/functions/login', requestInit)
+      const { access_token: accessToken } = await response.json()
+      if (!accessToken) throw Error('Without access token')
+      dispatch(UpdateData, { message: 'Redirecting...' })
+      setTimeout(redirecting, 1000)
+    } catch (e) {
+      alert(e.message)
+      dispatch(WithoutMessage)
+    }
   }]
   ]
 }
 
 const InitialState = () => [
-  { loading: true, username: 'example', password: '12345678' },
+  { message: 'Loading...', username: 'example', password: '12345678' },
   [async dispatch => {
     const headers = new Headers()
     headers.append('Content-Type', 'application/json')
@@ -41,43 +48,52 @@ const InitialState = () => [
     }
     try {
       const response = await fetch('/.netlify/functions/tokens', requestInit)
-      const { access_token: accessToken, username } = await response.json()
+      const { access_token: accessToken } = await response.json()
       if (!accessToken) throw Error('Without access token')
-      dispatch(StoppedLoading, { accessToken, username, password: '' })
+      dispatch(UpdateData, { message: 'Redirecting...' })
+      setTimeout(redirecting, 1000)
     } catch (e) {
-      dispatch(StoppedLoading)
-      console.log(e)
+      console.log(e.message)
+      dispatch(WithoutMessage)
     }
   }]
 ]
 
-const StoppedLoading = (state, data) => {
-  return ({ ...state, loading: false, ...data })
+const WithoutMessage = (state, data) => {
+  return ({ ...state, message: '', ...data })
+}
+
+const UpdateData = (state, data) => {
+  return ({ ...state, ...data })
+}
+
+const redirecting = () => {
+  const urlSearchParams = new URLSearchParams(window.location.search)
+  const params = Object.fromEntries(urlSearchParams.entries())
+  let redirectURL = 'https://tcc.jonloureiro.dev'
+  if (params && params.callback) { redirectURL = atob(params.callback) }
+  window.location.href = redirectURL
 }
 
 app({
   init: InitialState,
-  view: ({ loading, username, password, accessToken }) => loading || accessToken
-    ? h('main', {}, [
-        loading && h('h1', {}, text('Loading')),
-        accessToken && h('div', {}, [
-          h('h1', {}, text('Sucess')),
-          h('h1', {}, text(`user: ${username}`))
-        ])
-      ])
-    : h('main', {}, [
-      h('h1', {}, text('Accounts')),
-      h('form', { onsubmit: OnSubmit }, [
-        h('div', { class: 'form-group' }, [
-          h('label', { for: 'username' }, text('Username')),
-          h('input', { type: 'text', value: username, id: 'username' })
-        ]),
-        h('div', { class: 'form-group' }, [
-          h('label', { for: 'password' }, text('Password')),
-          h('input', { type: 'password', value: password, id: 'password' })
-        ]),
-        h('input', { type: 'submit', value: 'Login' })
-      ])
-    ]),
+  view: ({ message, username, password }) =>
+    h('main', {}, message
+      ? [h('h1', {}, text(message))]
+      : [
+          h('h1', {}, text('Accounts')),
+          h('form', { onsubmit: OnSubmit }, [
+            h('div', { class: 'form-group' }, [
+              h('label', { for: 'username' }, text('Username')),
+              h('input', { type: 'text', value: username, id: 'username' })
+            ]),
+            h('div', { class: 'form-group' }, [
+              h('label', { for: 'password' }, text('Password')),
+              h('input', { type: 'password', value: password, id: 'password' })
+            ]),
+            h('input', { type: 'submit', value: 'Login' })
+          ])
+        ]
+    ),
   node: document.getElementById('app')
 })
